21b0b7c8220b34b8dfb91cc309a31ee237be4581
unknown <boninoffranco@gmail.com>
2021-08-20 20:20:11 -0300

    Change projects tab style and responsiveness

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,7 +4,9 @@ import movies from "../../assets/movies.webp";
 import countries from "../../assets/countries.webp";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 import { FiArrowUpRight } from "react-icons/fi";
+import { IconContext } from "react-icons/lib";
 
 const Container = styled.div`
 	background: ${props => props.theme.background};
@@ -13,15 +15,27 @@ const Container = styled.div`
 `;
 
 const Project = styled.div`
-	height: 22vh;
-	width: 20vw;
-	min-width: 17rem;
-	min-height: 10rem;
-	box-shadow: 0 0 0.4rem ${props => props.theme.color};
-	margin: 4vw;
-	backgroundColor: ${props => props.theme.backgroundColor};
+	box-shadow: 0 0 0.3rem ${props => props.theme.primary};
+	background-color: ${props => props.theme.backgroundColor};
 	color: ${props => props.theme.color};
-	position: relative;
+`;
+
+const Card = styled.div`
+	background-color: ${props => props.theme.backgroundColor};
+`;
+
+const Link = styled.a`
+	color: ${props => props.theme.color};
+	border: 2px solid ${props => props.theme.color};
+	&:hover {
+		background-color: ${props => props.theme.primary};
+	}
+`;
+
+const Techs = styled.div`
+	background-color: ${props => props.theme.secondary};
+	border-top: 2px solid ${props => props.theme.primary};
+	color: ${props => props.theme.backgroundColor};
 `;
 
 const Projects = ({ theme }) => {
@@ -29,93 +43,95 @@ const Projects = ({ theme }) => {
 
 	return (
 		<Container className={style.container} id="projects">
-			<h1 className={style.title}> {t("projects.title")} </h1>
-			<div className={style.projects}>
-				<Project className={style.project}>
-					<div className={style.cardDiv}>
-						<h1> AltaBirra </h1>
-						<div className={style.linksDiv}>
-							<a href="https://alta-birra.vercel.app" className={style.links}>
-								< FiArrowUpRight /> {t("projects.visit")}
-							</a>
-							<a href="https://github.com/FBonino/Funtivity" className={style.links}>
-								<img src="https://image.flaticon.com/icons/png/32/733/733609.png" alt="" height="16px" width="16px" /> {t("projects.more")}
-							</a>
+			<IconContext.Provider value={{ size: 36, color: theme.color }}>
+				<h1 className={style.title}> {t("projects.title")} </h1>
+				<div className={style.projects}>
+					<Project className={style.project}>
+						<Card className={style.cardDiv}>
+							<h1 className={style.projectTitle}> AltaBirra </h1>
+							<div className={style.linksDiv}>
+								<Link href="https://github.com/FBonino/Funtivity" className={style.links}>
+									<FaGithub /> {t("projects.more")}
+								</Link>
+								<Link href="https://alta-birra.vercel.app" className={style.links}>
+									<FiArrowUpRight /> {t("projects.visit")}
+								</Link>
+							</div>
+							<Techs className={style.technologies}>
+								<p> Typescript </p>
+								<p> React </p>
+								<p> Express.js </p>
+								<p> Prisma </p>
+							</Techs>
+						</Card>
+						<div className={style.cardImage}>
+							<img className={style.projectImage} src={countries} alt="" />
 						</div>
-						<div className={style.technologies}>
-							<p> Typescript </p>
-							<p> React </p>
-							<p> Express.js </p>
-							<p> Prisma </p>
+					</Project>
+					<Project className={style.project}>
+						<Card className={style.cardDiv}>
+							<h1 className={style.projectTitle}> Funtivity </h1>
+							<div className={style.linksDiv}>
+								<Link href="https://github.com/FBonino/Funtivity" className={style.links}>
+									<FaGithub /> {t("projects.more")}
+								</Link>
+								<Link href="http://funtivity.vercel.app" className={style.links}>
+									<FiArrowUpRight /> {t("projects.visit")}
+								</Link>
+							</div>
+							<Techs className={style.technologies}>
+								<p> React </p>
+								<p> Redux </p>
+								<p> Node.js </p>
+								<p> Express.js </p>
+							</Techs>
+						</Card>
+						<div className={style.cardImage}>
+							<img className={style.projectImage} src={countries} alt="" />
 						</div>
-					</div>
-					<div className={style.cardImage}>
-						<img className={style.projectImage} src={countries} alt="" />
-					</div>
-				</Project>
-				<Project className={style.project}>
-					<div className={style.cardDiv}>
-						<h1> Funtivity </h1>
-						<div className={style.linksDiv}>
-							<a href="http://funtivity.vercel.app" className={style.links}>
-								< FiArrowUpRight /> {t("projects.visit")}
-							</a>
-							<a href="https://github.com/FBonino/Funtivity" className={style.links}>
-								<img src="https://image.flaticon.com/icons/png/32/733/733609.png" alt="" height="16px" width="16px" /> {t("projects.more")}
-							</a>
+					</Project>
+					<Project className={style.project}>
+						<Card className={style.cardDiv}>
+							<h1 className={style.projectTitle}> MoviesApp </h1>
+							<div className={style.linksDiv}>
+								<Link href="https://github.com/FBonino/MoviesApp" className={style.links}>
+									<FaGithub /> {t("projects.more")}
+								</Link>
+								<Link href="https://movies-app-fbonino.vercel.app" className={style.links}>
+									<FiArrowUpRight /> {t("projects.visit")}
+								</Link>
+							</div>
+							<Techs className={style.technologies}>
+								<p> React </p>
+								<p> Redux </p>
+							</Techs>
+						</Card>
+						<div className={style.cardImage}>
+							<img className={style.projectImage} src={movies} alt="" />
 						</div>
-						<div className={style.technologies}>
-							<p> React </p>
-							<p> Redux </p>
-							<p> Node.js </p>
-							<p> Express.js </p>
+					</Project>
+					<Project className={style.project}>
+						<Card className={style.cardDiv}>
+							<h1 className={style.projectTitle}> WeatherApp </h1>
+							<div className={style.linksDiv}>
+								<Link href="https://github.com/FBonino/WeatherApp" className={style.links}>
+									<FaGithub /> {t("projects.more")}
+								</Link>
+								<Link href="https://weather-app-fbonino.vercel.app" className={style.links}>
+									<FiArrowUpRight /> {t("projects.visit")}
+								</Link>
+							</div>
+							<Techs className={style.technologies}>
+								<p> React </p>
+								<p> CSS </p>
+							</Techs>
+						</Card>
+						<div className={style.cardImage}>
+							<img className={style.projectImage} src={weather} alt="" />
 						</div>
-					</div>
-					<div className={style.cardImage}>
-						<img className={style.projectImage} src={countries} alt="" />
-					</div>
-				</Project>
-				<Project className={style.project}>
-					<div className={style.cardDiv}>
-						<h1> MoviesApp </h1>
-						<div className={style.linksDiv}>
-							<a href="https://movies-app-fbonino.vercel.app" className={style.links}>
-								< FiArrowUpRight /> {t("projects.visit")}
-							</a>
-							<a href="https://github.com/FBonino/MoviesApp" className={style.links}>
-								<img src="https://image.flaticon.com/icons/png/32/733/733609.png" alt="" height="16px" width="16px" /> {t("projects.more")}
-							</a>
-						</div>
-						<div className={style.technologies}>
-							<p> React </p>
-							<p> Redux </p>
-						</div>
-					</div>
-					<div className={style.cardImage}>
-						<img className={style.projectImage} src={movies} alt="" />
-					</div>
-				</Project>
-				<Project className={style.project}>
-					<div className={style.cardDiv}>
-						<h1> WeatherApp </h1>
-						<div className={style.linksDiv}>
-							<a href="https://weather-app-fbonino.vercel.app" className={style.links}>
-								< FiArrowUpRight /> {t("projects.visit")}
-							</a>
-							<a href="https://github.com/FBonino/WeatherApp" className={style.links}>
-								<img src="https://image.flaticon.com/icons/png/32/733/733609.png" alt="" height="16px" width="16px" /> {t("projects.more")}
-							</a>
-						</div>
-						<div className={style.technologies}>
-							<p> React </p>
-							<p> CSS </p>
-						</div>
-					</div>
-					<div className={style.cardImage}>
-						<img className={style.projectImage} src={weather} alt="" />
-					</div>
-				</Project>
-			</div>
+					</Project>
+				</div>
+			</IconContext.Provider>
 		</Container>
 	)
 }
