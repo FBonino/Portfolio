d1e91d9e9415807ef43a7c8837bcc2d6fc73ac26
unknown <boninoffranco@gmail.com>
2021-08-22 11:09:03 -0300

    Fix service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,9 @@
 import { clientsClaim } from 'workbox-core';
-// import { ExpirationPlugin } from 'workbox-expiration';
+import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-// import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
-// import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 clientsClaim();
 
@@ -26,27 +26,27 @@ registerRoute(
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
-// registerRoute(
-//   ({ request }) => request.destination === 'script' ||
-//     request.destination === 'style',
-//   new StaleWhileRevalidate()
-// );
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+    request.destination === 'style',
+  new StaleWhileRevalidate()
+);
 
-// registerRoute(
-//   ({ request }) => request.destination === 'image',
-//   new CacheFirst({
-//     cacheName: 'images',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [0, 200],
-//       }),
-//       new ExpirationPlugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 30 * 24 * 60 * 60,
-//       }),
-//     ],
-//   }),
-// );
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  }),
+);
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
